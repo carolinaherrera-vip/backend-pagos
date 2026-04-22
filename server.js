c1b871b468d1dbcbcf55b6820dc0c0114fcf8bbf
CarolinaHerrera-VIP <iikingvic30@gmail.com>
2026-04-22 01:32:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -279,6 +279,8 @@ bot.on("callback_query", async (query) => {
 });
 
 // ================================
-app.listen(3000, () => {
-    console.log("🚀 Servidor corriendo en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("🚀 Servidor corriendo en puerto", PORT);
 });
